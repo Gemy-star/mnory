8905e3497bca7e5b27b8f8058d0c02b038213c72
Mohamed Ismail <mohamed.g.ismail@orange.com>
2025-08-23 15:35:51 +0300

    add email alert

--- mnory/settings/local.py
+++ mnory/settings/local.py
@@ -1,20 +1,9 @@
 from .common import *
 
 
-INSTALLED_APPS += [
-    'silk',
-]
+# INSTALLED_APPS += [
+#     'silk',
+# ]
 
-MIDDLEWARE += ['silk.middleware.SilkyMiddleware',]
+# MIDDLEWARE += ['silk.middleware.SilkyMiddleware',]
 
-
-PYTHONANYWHERE_DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mnory$mnorydb', # IMPORTANT: Use your actual username and database name
-        'USER': 'mnory',                  # Your PythonAnywhere username
-        'PASSWORD': 'admin123456',       # The password you set on the Databases tab
-        'HOST': 'mnory.mysql.pythonanywhere-services.com', # The hostname from the Databases tab
-        'PORT': '',                              # Leave empty for default MySQL port (3306)
-    }
-}
