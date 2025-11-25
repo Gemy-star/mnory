a307e51eb17255ed1040c927540fbea3a2ab26e9
Mohamed Ismail <mohamed.g.ismail@orange.com>
2025-11-25 11:14:37 +0200

    add new data

--- mnory/settings/common.py
+++ mnory/settings/common.py
@@ -139,6 +139,7 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                "django.template.context_processors.i18n",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "shop.context_processors.categories_processor",
